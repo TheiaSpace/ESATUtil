2c4088d2a2ef1e9e74346af21a09626d7beebdb1
Sergio García-Cuevas González <sgarcia@eusoc.upm.es>
2017-10-10 15:02:30 +0200

    Comments about movements of the read/byte pointer.

--- ESATCCSDSPacket.h
+++ ESATCCSDSPacket.h
@@ -94,6 +94,7 @@ class ESATCCSDSPacket: public Printable
     boolean endOfPacketDataReached();
 
     // Print the packet in human-readable form.
+    // This leaves the read/byte pointer untouched.
     size_t printTo(Print& output) const;
 
     // Read the CCSDS application process identifier.
@@ -108,13 +109,16 @@ class ESATCCSDSPacket: public Printable
     // Read the next boolean (an 8-bit entry) from the packet data.
     // The raw datum is stored as a 0 for false and as any other
     // 8-bit number for true.
+    // This advances the read/byte pointer by 1.
     boolean readBoolean();
 
     // Read the next 8-bit signed integer from the packet data.
     // The raw datum is stored in two's complement format.
+    // This advances the read/byte pointer by 1.
     signed char readChar();
 
     // Read the next 8-bit unsigned integer from the packet data.
+    // This advances the read/byte pointer by 1.
     byte readByte();
 
     // Read the next single-precision floating-point number
@@ -122,20 +126,24 @@ class ESATCCSDSPacket: public Printable
     // The raw datum is stored in big-endian byte order, IEEE 754
     // format, single-precision (32-bit, binary32).
     // Denormal numbers are not properly handled.
+    // This advances the read/byte pointer by 4.
     float readFloat();
 
     // Fill the packet with incoming data from an input stream.
     // Return true on success; false otherwise.
+    // This leaves the read/byte pointer untouched.
     boolean readFrom(Stream& input);
 
     // Read the next 16-bit signed integer from the packet data.
     // The raw datum is stored in big-endian byte order,
     // two's complement format.
+    // This advances the read/byte pointer by 2.
     int readInt();
 
     // Read the next 32-bit signed integer from the packet data.
     // The raw datum is stored in bin-endian byte order,
     // two's complement format.
+    // This advances the read/byte pointer by 4.
     long readLong();
 
     // Return the packet data length (expressed in octets).
@@ -146,6 +154,7 @@ class ESATCCSDSPacket: public Printable
     // The raw datum is stored as a 16-bit unsigned integer
     // in big-endian byte order, as the actual packet data
     // length minus 1.
+    // This leaves the read/byte pointer untouched.
     long readPacketDataLength() const;
 
     // Read the CCSDS packet sequence count.
@@ -154,18 +163,21 @@ class ESATCCSDSPacket: public Printable
     // and it should be incremented every time a new packet is generated.
     // The raw datum is stored as a 14-bit unsigned integer,
     // most significant bit first.
+    // This leaves the read/byte pointer untouched.
     word readPacketSequenceCount() const;
 
     // Read the CCSDS packet type: either telemetry or telecommand.
     // This field is part of the primary header.
     // The raw datum is stored as a bit:
     // 0 for TELEMETRY, 1 for TELECOMMAND.
+    // This leaves the read/byte pointer untouched.
     PacketType readPacketType() const;
 
     // Return the CCSDS packet version number.
     // This field is part of the primary header.
     // The raw datum is stored as a 3-bit unsigned integer, most
     // significant bit first.
+    // This leaves the read/byte pointer untouched.
     byte readPacketVersionNumber() const;
 
     // Read the CCSDS secondary header flag.
@@ -173,6 +185,7 @@ class ESATCCSDSPacket: public Printable
     // The raw datum is stored as a bit:
     // 0 for SECONDARY_HEADER_IS_NOT_PRESENT,
     // 1 for SECONDARY_HEADER_IS_PRESENT.
+    // This leaves the read/byte pointer untouched.
     SecondaryHeaderFlag readSecondaryHeaderFlag() const;
 
     // Read the CCSDS sequence flags.
@@ -183,18 +196,21 @@ class ESATCCSDSPacket: public Printable
     // 1 for FIRST_SEGMENT_OF_USER_DATA,
     // 2 for LAST_SEGMENT_OF_USER_DATA,
     // 3 for UNSEGMENTED_USER_DATA.
+    // This leaves the read/byte pointer untouched.
     SequenceFlags readSequenceFlags() const;
 
     // Read the next 32-bit unsigned integer from the packet data.
     // The raw datum is stored in big-endian byte order.
+    // This advances the read/byte pointer by 4.
     unsigned long readUnsignedLong();
 
     // Read the next 16-bit unsigned integer from the packet data.
     // The raw datum is stored in big-endian byte order.
+    // This advances the read/byte pointer by 2.
     word readWord();
 
-    // Move the read/write pointer back to the start of the packet data
-    // field (packet payload).
+    // Move the read/write pointer to 0: back to the start of the
+    // packet data field (packet payload).
     void rewind();
 
     // Update the packet data length to match the number of bytes written
@@ -204,6 +220,7 @@ class ESATCCSDSPacket: public Printable
     // If the number of bytes written to the packet is 0, the packet
     // data length will be erroneous, but empty CCSDS packets
     // are forbidden by the standard.
+    // This leaves the read/byte pointer untouched.
     void updatePacketDataLength();
 
     // Write the CCSDS application process identifier.
@@ -213,35 +230,40 @@ class ESATCCSDSPacket: public Printable
     // subsystem should have its own application process identifier).
     // The raw datum is stored as an 11-bit unsigned integer,
     // most significant bit first.
+    // This leaves the read/byte pointer untouched.
     void writeApplicationProcessIdentifier(word applicationProcessIdentifier);
 
     // Append a boolean to the packet data.
     // The raw datum is stored as an 8-bit unsigned integer:
     // 0 for false, 1 for true.
-    // This increments the packet data length by 1.
+    // This advances the read/byte pointer by 1.
     void writeBoolean(boolean datum);
 
     // Append an 8-bit unsigned integer to the packet data.
-    // This increments the packet data length by 1.
+    // This advances the read/byte pointer by 1.
     void writeByte(byte datum);
 
     // Append an 8-bit signed integer to the packet data.
+    // This advances the read/byte pointer by 1.
     void writeChar(signed char datum);
 
     // Append a floating-point number to the packet data.
     // The raw datum is stored in big-endian byte order, IEEE 754
     // format, single-precision (32-bit, binary32).
     // Denormal numbers are not properly handled.
+    // This advances the read/byte pointer by 4.
     void writeFloat(float datum);
 
     // Append a 16-bit signed integer to the packet data.
     // The raw datum is stored in big-endian byte order,
     // two's complement format.
+    // This advances the read/byte pointer by 2.
     void writeInt(int datum);
 
     // Append a 32-bit signed integer to the packet data.
     // The raw datum is stored in big-endian byte order,
     // two's complement format.
+    // This advances the read/byte pointer by 4.
     void writeLong(long datum);
 
     // Write the packet data length (expressed in octets).
@@ -254,6 +276,7 @@ class ESATCCSDSPacket: public Printable
     // length minus 1.  As valid packet data lengths go from
     // 1 to 65536 (both included), the behaviour when given
     // an argument outside of that interval is undefined.
+    // This leaves the read/byte pointer untouched.
     void writePacketDataLength(long packetDataLength);
 
     // Read the CCSDS packet sequence count.
@@ -262,6 +285,7 @@ class ESATCCSDSPacket: public Printable
     // and it should be incremented every time a new packet is generated.
     // The raw datum is stored as a 14-bit unsigned integer,
     // most significant bit first.
+    // This leaves the read/byte pointer untouched.
     void writePacketSequenceCount(word packetSequenceCount);
 
     // Write the CCSDS packet type: wether telemetry or telecommand.
@@ -270,6 +294,7 @@ class ESATCCSDSPacket: public Printable
     // 0 for TELEMETRY, 1 for TELECOMMAND.
     // The behaviour with arguments other than TELEMETRY or TELECOMMAND
     // is undefined.
+    // This leaves the read/byte pointer untouched.
     void writePacketType(PacketType packetType);
 
     // Write the CCSDS packet version number.
@@ -281,6 +306,7 @@ class ESATCCSDSPacket: public Printable
     // As valid packet version numbers go from 0 to 7 (both included),
     // the behaviour when given an argument outside of that interval
     // is undefined.
+    // This leaves the read/byte pointer untouched.
     void writePacketVersionNumber(byte packetVersionNumber);
 
     // Write the CCSDS secondary header flag.
@@ -290,6 +316,7 @@ class ESATCCSDSPacket: public Printable
     // 1 for SECONDARY_HEADER_IS_PRESENT.
     // The behaviour with arguments other than SECONDARY_HEADER_IS_NOT_PRESENT
     // or SECONDARY_HEADER_IS_PRESENT is undefined.
+    // This leaves the read/byte pointer untouched.
     void writeSecondaryHeaderFlag(SecondaryHeaderFlag secondaryHeaderFlag);
 
     // Write the CCSDS sequence flags.
@@ -303,20 +330,25 @@ class ESATCCSDSPacket: public Printable
     // The behaviour with arguments other than
     // CONTINUATION_SEGMENT_OF_USER_DATA, FIRST_SEGMENT_OF_USER_DATA,
     // LAST_SEGMENT_OF_USER_DATA or UNSEGMENTED_USER_DATA is undefined.
+    // This leaves the read/byte pointer untouched.
     void writeSequenceFlags(SequenceFlags sequenceFlags);
 
     // Write the raw contents of the packet to an output stream.
     // Return true on success; otherwise return false.
+    // This leaves the read/byte pointer untouched.
     boolean writeTo(Stream& output);
 
     // Append a 32-bit unsigned integer to the packet data.
     // This increments the packet data length by 4.
     // The raw datum is stored in big-endian byte order.
+    // This advances the read/byte pointer by 1.
+    // This advances the read/byte pointer by 4.
     void writeUnsignedLong(unsigned long datum);
 
     // Append a 16-bit unsigned integer to the packet data.
     // This increments the packet data length by 2.
     // The raw datum is stored in big-endian byte order.
+    // This advances the read/byte pointer by 2.
     void writeWord(word datum);
 
   private:
