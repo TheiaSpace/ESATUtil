0267eebffa4d7a7330564161d52549273c3311c3
Sergio García-Cuevas González <sgarcia@eusoc.upm.es>
2017-10-16 21:02:56 +0200

    Consistent spacing.

--- ESATUtil.cpp
+++ ESATUtil.cpp
@@ -71,63 +71,63 @@ word ESATUtil::hexadecimalToWord(const String hexadecimalNumber) const
     byte digit = 0;
     switch (character)
     {
-    case '0':
-      digit = 0;
-      break;
-    case '1':
-      digit = 1;
-      break;
-    case '2':
-      digit = 2;
-      break;
-    case '3':
-      digit = 3;
-      break;
-    case '4':
-      digit = 4;
-      break;
-    case '5':
-      digit = 5;
-      break;
-    case '6':
-      digit = 6;
-      break;
-    case '7':
-      digit = 7;
-      break;
-    case '8':
-      digit = 8;
-      break;
-    case '9':
-      digit = 9;
-      break;
-    case 'a':
-    case 'A':
-      digit = 0xa;
-      break;
-    case 'b':
-    case 'B':
-      digit = 0xb;
-      break;
-    case 'c':
-    case 'C':
-      digit = 0xc;
-      break;
-    case 'd':
-    case 'D':
-      digit = 0xd;
-      break;
-    case 'e':
-    case 'E':
-      digit = 0xe;
-      break;
-    case 'f':
-    case 'F':
-      digit = 0xf;
-      break;
-    default:
-      return 0;
-      break;
+      case '0':
+        digit = 0;
+        break;
+      case '1':
+        digit = 1;
+        break;
+      case '2':
+        digit = 2;
+        break;
+      case '3':
+        digit = 3;
+        break;
+      case '4':
+        digit = 4;
+        break;
+      case '5':
+        digit = 5;
+        break;
+      case '6':
+        digit = 6;
+        break;
+      case '7':
+        digit = 7;
+        break;
+      case '8':
+        digit = 8;
+        break;
+      case '9':
+        digit = 9;
+        break;
+      case 'a':
+      case 'A':
+        digit = 0xa;
+        break;
+      case 'b':
+      case 'B':
+        digit = 0xb;
+        break;
+      case 'c':
+      case 'C':
+        digit = 0xc;
+        break;
+      case 'd':
+      case 'D':
+        digit = 0xd;
+        break;
+      case 'e':
+      case 'E':
+        digit = 0xe;
+        break;
+      case 'f':
+      case 'F':
+        digit = 0xf;
+        break;
+      default:
+        return 0;
+        break;
     }
     number = number * 0x10 + digit;
   }
