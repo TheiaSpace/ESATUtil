274170f34dd877f59f86f2bf9487c411a28ea21b
Sergio García-Cuevas González <sgarcia@eusoc.upm.es>
2018-03-19 14:24:30 +0100

    New ESAT_Clock interface.

---
+++ src/ESAT_Clock.h
@@ -0,0 +1,38 @@
+/*
+ * This file is part of Theia Space's ESAT Util library.
+ *
+ * Theia Space's ESAT Util library is free software: you can
+ * redistribute it and/or modify it under the terms of the GNU General
+ * Public License as published by the Free Software Foundation, either
+ * version 3 of the License, or (at your option) any later version.
+ *
+ * Theia Space's ESAT Util library is distributed in the hope that it
+ * will be useful, but WITHOUT ANY WARRANTY; without even the implied
+ * warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+ * See the GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with Theia Space's ESAT Util library.  If not, see
+ * <http://www.gnu.org/licenses/>.
+ */
+
+#include <Arduino.h>
+#include "ESAT_Timestamp.h"
+
+#ifndef ESAT_Clock_h
+#define ESAT_Clock_h
+
+// Real-time clock interface.
+class ESAT_Clock
+{
+  public:
+    virtual ~ESAT_Clock() {};
+
+    // Return the current timestamp.
+    virtual ESAT_Timestamp read() = 0;
+
+    // Set the time to the given timestamp.
+    void write(ESAT_Timestamp timestamp);
+};
+
+#endif /* ESAT_Clock_h */
