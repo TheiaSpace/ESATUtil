2e1c5f43749aeccb6209fa455fe1815bff418049
Sergio García-Cuevas González <sgarcia@eusoc.upm.es>
2018-03-20 16:53:48 +0100

    Packet contents interface.

---
+++ src/ESAT_CCSDSPacketContents.h
@@ -0,0 +1,43 @@
+/*
+ * This file is part of Theia Space's ESAT Util library.
+ *
+ * Theia Space's ESAT Util library is free software: you can
+ * redistribute it and/or modify it under the terms of the GNU General
+ * Public License as published by the Free Software Foundation, either
+ * version 3 of the License, or (at your option) any later version.
+ *
+ * Theia Space's ESAT Util library is distributed in the hope that it
+ * will be useful, but WITHOUT ANY WARRANTY; without even the implied
+ * warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+ * See the GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with Theia Space's ESAT Util library.  If not, see
+ * <http://www.gnu.org/licenses/>.
+ */
+
+#ifndef ESAT_CCSDSPacketContents_h
+#define ESAT_CCSDSPacketContents_h
+
+#include <Arduino.h>
+#include "ESAT_CCSDSPacket.h"
+
+// Packet contents interface.
+// Pass ESAT_CCSDSPacketContents to
+// ESAT_CCSDSPacketBuilder.buildPacket() to build CCSDS packets.
+class ESAT_CCSDSPacketContents
+{
+  public:
+    virtual ~ESAT_CCSDSPacketContents() {};
+
+    // Return the packet identifier.
+    virtual byte packetIdentifier() = 0;
+
+    // Fill the user data field of the given packet.
+    // The write pointer of the packet is already at the start
+    // of the user data field.
+    // Return true on success; otherwise return false.
+    virtual boolean fillUserData(ESAT_CCSDSPacket& packet) = 0;
+};
+
+#endif /* ESAT_CCSDSPacketContents_h */
