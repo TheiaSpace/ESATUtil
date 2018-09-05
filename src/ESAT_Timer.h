fd99ae0c0698209729b0eafe6b425bc7a6148d7f
Sergio García-Cuevas González <sgarcia@eusoc.upm.es>
2018-09-05 16:21:25 +0200

    Copyright note for ESAT_Timer.

--- src/ESAT_Timer.h
+++ src/ESAT_Timer.h
@@ -1,4 +1,6 @@
 /*
+ * Copyright (C) 2017 Theia Space, Universidad Politécnica de Madrid
+ *
  * This file is part of Theia Space's ESAT Util library.
  *
  * Theia Space's ESAT Util library is free software: you can
