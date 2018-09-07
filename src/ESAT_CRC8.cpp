e4a5f74576737812b70f7395824840692d8eb197
Sergio García-Cuevas González <sgarcia@eusoc.upm.es>
2018-09-07 13:28:40 +0200

    This is part of ESAT Util, not ESAT OBC.

--- src/ESAT_CRC8.cpp
+++ src/ESAT_CRC8.cpp
@@ -14,7 +14,7 @@
  * See the GNU General Public License for more details.
  *
  * You should have received a copy of the GNU General Public License
- * along with Theia Space's ESAT OBC library.  If not, see
+ * along with Theia Space's ESAT Util library.  If not, see
  * <http://www.gnu.org/licenses/>.
  */
 
