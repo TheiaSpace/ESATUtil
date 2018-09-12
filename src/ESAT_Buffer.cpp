3f3f0c2da7138558865805d0d869c41d12bdc4c6
Sergio García-Cuevas González <sgarcia@eusoc.upm.es>
2018-09-12 11:09:55 +0200

    Comments on the implementation of ESAT_Buffer.

--- src/ESAT_Buffer.cpp
+++ src/ESAT_Buffer.cpp
@@ -26,6 +26,8 @@ ESAT_Buffer::ESAT_Buffer()
   bufferCapacity = 0;
   bytesInBuffer = 0;
   readWritePosition = 0;
+  // Set the timeout for waiting for stream data to zero, as it
+  // doesn't make sense to wait when reading from these buffers.
   setTimeout(0);
 }
 
@@ -35,11 +37,15 @@ ESAT_Buffer::ESAT_Buffer(byte array[], const unsigned long length)
   bufferCapacity = length;
   bytesInBuffer = 0;
   readWritePosition = 0;
+  // Set the timeout for waiting for stream data to zero, as it
+  // doesn't make sense to wait when reading from these buffers.
   setTimeout(0);
 }
 
 int ESAT_Buffer::available()
 {
+  // Truncate the result of availableBytes() to fit a 16-bit signed
+  // integer.
   return min(availableBytes(), 0x7FFF);
 }
 
@@ -62,6 +68,7 @@ unsigned long ESAT_Buffer::capacity() const
 
 void ESAT_Buffer::flush()
 {
+  // Reset the buffer length and rewind.
   bytesInBuffer = 0;
   readWritePosition = 0;
 }
@@ -73,6 +80,7 @@ unsigned long ESAT_Buffer::length() const
 
 int ESAT_Buffer::peek()
 {
+  // Peeking past the last available byte returns -1.
   if (availableBytes() == 0)
   {
     return -1;
@@ -87,10 +95,12 @@ unsigned long ESAT_Buffer::position() const
 
 size_t ESAT_Buffer::printTo(Print& output) const
 {
+  // Fall through when the buffer is empty.
   if (bytesInBuffer == 0)
   {
     return 0;
   }
+  // Normal operation: print the contents of the buffer.
   size_t bytesWritten =
     output.print(String(buffer[0], HEX));
   for (unsigned long i = 1; i < bytesInBuffer; i++)
@@ -108,6 +118,8 @@ size_t ESAT_Buffer::printTo(Print& output) const
 int ESAT_Buffer::read()
 {
   const int datum = peek();
+  // Advance the read/write position only when there is a byte
+  // available.
   if (datum > -1)
   {
     readWritePosition = readWritePosition + 1;
@@ -117,15 +129,21 @@ int ESAT_Buffer::read()
 
 boolean ESAT_Buffer::readFrom(Stream& input, const unsigned long bytesToRead)
 {
+  // As we flush the buffer first, we will lose the original state of
+  // the buffer even if the read fails.
   flush();
+  // Just fail if we cannot fit the required number of bytes into the
+  // buffer.
   if (bytesToRead > bufferCapacity)
   {
     return false;
   }
+  // Fall through when the required number of bytes is 0.
   if (bytesToRead == 0)
   {
     return true;
   }
+  // Normal operation: read the required bytes from the input stream.
   bytesInBuffer = input.readBytes((char*) buffer, bytesToRead);
   if (bytesInBuffer == bytesToRead)
   {
@@ -144,14 +162,19 @@ void ESAT_Buffer::rewind()
 
 size_t ESAT_Buffer::write(const uint8_t datum)
 {
+  // Just fail if we have no backend buffer.
   if (!buffer)
   {
     return 0;
   }
+  // Just fail if we are above capacity.
   if (readWritePosition >= bufferCapacity)
   {
     return 0;
   }
+  // Normal operation: write the datum to the current read/write
+  // position of the backend buffer, increment the counters and return
+  // the number of written bytes (1).
   buffer[readWritePosition] = datum;
   readWritePosition = readWritePosition + 1;
   bytesInBuffer = readWritePosition;
@@ -160,14 +183,17 @@ size_t ESAT_Buffer::write(const uint8_t datum)
 
 boolean ESAT_Buffer::writeTo(Stream& output) const
 {
+  // Just fail if we have no backend buffer.
   if (!buffer)
   {
     return false;
   }
+  // Avoid writing if the buffer is empty.
   if (bytesInBuffer == 0)
   {
     return true;
   }
+  // Normal operation: dump the contents of the buffer.
   const size_t bytesWritten = output.write(buffer, bytesInBuffer);
   if (bytesWritten < bytesInBuffer)
   {
