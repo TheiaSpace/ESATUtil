17f915f2a2a25222e0f672670b2d230e2b51feb5
Sergio García-Cuevas González <sgarcia@eusoc.upm.es>
2018-09-24 14:02:16 +0200

    Add SoftwareClock example.

---
+++ examples/SoftwareClock/SoftwareClock.ino
@@ -0,0 +1,48 @@
+/*
+ * Copyright (C) 2018 Theia Space, Universidad Politécnica de Madrid
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+#include <ESAT_SoftwareClock.h>
+
+// ESAT_SoftwareCLock example program.
+// Basic timekeeping.
+
+ESAT_SoftwareClock clock;
+// Start the clock at the time of launch of the first artificial
+// satellite (Sputnik 1).
+ESAT_Timestamp startTime(1957, 10, 4, 19, 28, 34);
+
+void setup()
+{
+  // Configure the Serial interface.
+  Serial.begin(9600);
+  // Wait until Serial is ready.
+  while (!Serial)
+  {
+  }
+  // Set the clock.
+  clock.write(startTime);
+}
+
+void loop()
+{
+  (void) Serial.println(String("Software clock example program."));
+  (void) Serial.print(String("Start time: "));
+  (void) Serial.println(startTime);
+  (void) Serial.print(String("Current time: "));
+  (void) Serial.println(clock.read());
+  delay(1000);
+}
