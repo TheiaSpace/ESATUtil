7dc9a1120c318a9b11a8557da2a008db42a7bea5
Sergio García-Cuevas González <sgarcia@eusoc.upm.es>
2018-09-24 16:11:57 +0200

    Add Buffer example.

---
+++ examples/Buffer/Buffer.ino
@@ -0,0 +1,79 @@
+/*
+ * Copyright (C) 2018 Theia Space, Universidad Politécnica de Madrid
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+#include <ESAT_Buffer.h>
+
+// ESAT_Buffer example program.
+// Stream interface for reading from and writing to buffers.
+
+// Work with this buffer.
+const byte capacity = 4;
+byte array[capacity];
+ESAT_Buffer buffer(array, capacity);
+
+void setup()
+{
+  // Configure the Serial interface.
+  Serial.begin(9600);
+  // Wait until Serial is ready.
+  while (!Serial)
+  {
+  }
+  // Seed the random number generator.
+  randomSeed(0);
+}
+
+void loop()
+{
+  (void) Serial.println(String("Buffer streams example program."));
+  // Clear the buffer.
+  (void) Serial.println(String("Clearing (flushing) the buffer..."));
+  buffer.flush();
+  // Fill the buffer.
+  (void) Serial.println(String("Filling the buffer with random data..."));
+  while (buffer.length() < buffer.capacity())
+  {
+    (void) buffer.write(random(0, 255));
+  }
+  // Print the buffer.
+  (void) Serial.print(String("Hexadecimal dump of the buffer: "));
+  (void) Serial.println(buffer);
+  (void) Serial.print(String("Buffer length: "));
+  (void) Serial.println(String(buffer.length(), DEC));
+  // Read the buffer byte by byte.
+  (void) Serial.println(String("Reading the buffer contents byte by byte..."));
+  buffer.rewind();
+  while (buffer.available() > 0)
+  {
+    (void) Serial.print(String("Available bytes: "));
+    (void) Serial.println(String(buffer.available(), DEC));
+    (void) Serial.print(String("Current byte: 0x"));
+    (void) Serial.println(String(buffer.read(), HEX));
+  }
+  // Direct access to the buffer contents.
+  (void) Serial.println(String("Accessing the backend array of the buffer..."));
+  for (unsigned long index = 0; index < buffer.length(); index++)
+  {
+    (void) Serial.print(String("array["));
+    (void) Serial.print(String(index, DEC));
+    (void) Serial.print(String("] = 0x"));
+    (void) Serial.println(String(array[index], HEX));
+  }
+  // End.
+  (void) Serial.println(String("End."));
+  delay(1000);
+}
---
+++ examples/KISSFrames/KISSFrames.ino
@@ -0,0 +1,105 @@
+/*
+ * Copyright (C) 2018 Theia Space, Universidad Politécnica de Madrid
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+#include <ESAT_Buffer.h>
+#include <ESAT_KISSStream.h>
+
+// ESAT_KISSStream example program.
+// KISS frame streams.
+
+// Input buffer.
+// Use an ESAT_Buffer object for pretty-printing.
+const byte inputCapacity = 4;
+byte inputBuffer[inputCapacity];
+ESAT_Buffer input(inputBuffer, inputCapacity);
+
+// Frame buffer.
+const byte frameCapacity = ESAT_KISSStream::frameLength(inputCapacity);
+byte frameBuffer[frameCapacity];
+ESAT_Buffer frame(frameBuffer, frameCapacity);
+
+// Decoded contents buffer.
+const byte decodedCapacity = inputCapacity;
+byte decodedBuffer[decodedCapacity];
+ESAT_Buffer decoded(decodedBuffer, decodedCapacity);
+
+// KISS frame streams.
+const byte readerCapacity = inputCapacity;
+byte readerBuffer[readerCapacity];
+ESAT_KISSStream kissReader(frame, readerBuffer, readerCapacity);
+ESAT_KISSStream kissWriter(frame);
+
+void setup()
+{
+  // Configure the Serial interface.
+  Serial.begin(9600);
+  // Wait until Serial is ready.
+  while (!Serial)
+  {
+  }
+  // Fill the input buffer with regular bytes and special bytes
+  // that must be escaped.
+  input.write(0x01);
+  input.write(0xC0);
+  input.write(0xDB);
+  input.write(0xFF);
+}
+
+void loop()
+{
+  (void) Serial.println(String("KISS frame streams example program."));
+  // Show the contents of the input buffer.
+  (void) Serial.print(String("Hexadecimal dump of the input: "));
+  (void) Serial.println(input);
+  // Write the contents of the input buffer to the KISS frame writer.
+  // The KISS frame writer writes to ESAT_Buffer frame.
+  (void) Serial.println(String("Starting a KISS frame..."));
+  (void) kissWriter.beginFrame();
+  (void) Serial.println(String("Writing the input to the KISS frame..."));
+  input.rewind();
+  while (input.available() > 0)
+  {
+    (void) kissWriter.write(input.read());
+  }
+  (void) Serial.println(String("Ending the KISS frame..."));
+  (void) kissWriter.endFrame();
+  // Print the KISS frame.
+  (void) Serial.print(String("Hexadecimal dump of the KISS frame: "));
+  (void) Serial.println(frame);
+  // Read the contents of the KISS frame reader.
+  // The KISS frame reader reads from ESAT_Buffer frame,
+  // so rewind frame to the KISS frame reader read from it.
+  frame.rewind();
+  (void) Serial.print(String("Receiving a frame..."));
+  const boolean gotFrame = kissReader.receiveFrame();
+  if (gotFrame)
+  {
+    (void) Serial.println(String("Hexadecimal dump of the decoded frame contents:"));
+    while (kissReader.available() > 0)
+    {
+      (void) decoded.write(kissReader.read());
+    }
+    (void) Serial.println(decoded);
+  }
+  else
+  {
+    (void) Serial.println(String("Couldn't receive a frame."));
+  }
+  // End.
+  (void) Serial.println(String("End."));
+  delay(1000);
+}
