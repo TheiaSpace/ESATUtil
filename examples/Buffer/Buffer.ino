6c17855bddf2899f507600641c60a05a1b7d4c8c
Sergio García-Cuevas González <sgarcia@eusoc.upm.es>
2018-09-25 13:59:38 +0200

    Remove explicit String conversions from Buffer example.

--- examples/Buffer/Buffer.ino
+++ examples/Buffer/Buffer.ino
@@ -39,44 +39,44 @@ void setup()
 
 void loop()
 {
-  (void) Serial.println(String("###############################"));
-  (void) Serial.println(String("Buffer streams example program."));
-  (void) Serial.println(String("###############################"));
+  (void) Serial.println("###############################");
+  (void) Serial.println("Buffer streams example program.");
+  (void) Serial.println("###############################");
   // Clear the buffer.
-  (void) Serial.println(String("Clearing (flushing) the buffer..."));
+  (void) Serial.println("Clearing (flushing) the buffer...");
   buffer.flush();
   // Fill the buffer.
-  (void) Serial.println(String("Filling the buffer with random data..."));
+  (void) Serial.println("Filling the buffer with random data...");
   while (buffer.length() < buffer.capacity())
   {
     (void) buffer.write(random(0, 256));
   }
   // Print the buffer.
-  (void) Serial.print(String("Hexadecimal dump of the buffer: "));
+  (void) Serial.print("Hexadecimal dump of the buffer: ");
   (void) Serial.println(buffer);
-  (void) Serial.print(String("Buffer length: "));
-  (void) Serial.println(String(buffer.length(), DEC));
+  (void) Serial.print("Buffer length: ");
+  (void) Serial.println(buffer.length(), DEC);
   // Read the buffer byte by byte.
-  (void) Serial.println(String("Reading the buffer contents byte by byte..."));
+  (void) Serial.println("Reading the buffer contents byte by byte...");
   buffer.rewind();
   while (buffer.available() > 0)
   {
-    (void) Serial.print(String("Available bytes: "));
-    (void) Serial.println(String(buffer.available(), DEC));
-    (void) Serial.print(String("Current byte: 0x"));
-    (void) Serial.println(String(buffer.read(), HEX));
+    (void) Serial.print("Available bytes: ");
+    (void) Serial.println(buffer.available(), DEC);
+    (void) Serial.print("Current byte: 0x");
+    (void) Serial.println(buffer.read(), HEX);
   }
   // Direct access to the buffer contents.
-  (void) Serial.println(String("Accessing the backend array of the buffer..."));
+  (void) Serial.println("Accessing the backend array of the buffer...");
   for (unsigned long index = 0; index < buffer.length(); index++)
   {
-    (void) Serial.print(String("array["));
-    (void) Serial.print(String(index, DEC));
-    (void) Serial.print(String("] = 0x"));
-    (void) Serial.println(String(array[index], HEX));
+    (void) Serial.print("array[");
+    (void) Serial.print(index, DEC);
+    (void) Serial.print("] = 0x");
+    (void) Serial.println(array[index], HEX);
   }
   // End.
-  (void) Serial.println(String("End."));
-  (void) Serial.println(String(""));
+  (void) Serial.println("End.");
+  (void) Serial.println("");
   delay(1000);
 }
