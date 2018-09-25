ef95ad5cdc5c874f6f228f7e7dc7e404f0ecace9
Sergio García-Cuevas González <sgarcia@eusoc.upm.es>
2018-09-25 14:04:30 +0200

    Remove explicit String conversions from CCSDSPacket example.

--- examples/CCSDSPacket/CCSDSPacket.ino
+++ examples/CCSDSPacket/CCSDSPacket.ino
@@ -55,14 +55,14 @@ void setup()
 
 void loop()
 {
-  (void) Serial.println(String("####################################"));
-  (void) Serial.println(String("CCSDS Space Packets example program."));
-  (void) Serial.println(String("####################################"));
+  (void) Serial.println("####################################");
+  (void) Serial.println("CCSDS Space Packets example program.");
+  (void) Serial.println("####################################");
   // Clear the packet.
-  (void) Serial.println(String("Clearing (flushing) the packet..."));
+  (void) Serial.println("Clearing (flushing) the packet...");
   packet.flush();
   // Fill the headers of a telemetry packet.
-  (void) Serial.println(String("Writing the headers of a telemetry packet..."));
+  (void) Serial.println("Writing the headers of a telemetry packet...");
   packet.writeTelemetryHeaders(applicationProcessIdentifier,
                                packetSequenceCount,
                                timestamp,
@@ -71,56 +71,56 @@ void loop()
                                patchVersionNumber,
                                packetIdentifier);
   // Fill the user data field.
-  (void) Serial.println(String("Filling the user data field..."));
-  (void) Serial.print(String("Writing floating-point number "));
-  (void) Serial.print(String(floatingPointNumber, 4));
-  (void) Serial.println(String("..."));
+  (void) Serial.println("Filling the user data field...");
+  (void) Serial.print("Writing floating-point number ");
+  (void) Serial.print(floatingPointNumber, 4);
+  (void) Serial.println("...");
   packet.writeFloat(floatingPointNumber);
-  (void) Serial.print(String("Writing integer number "));
-  (void) Serial.print(String(integerNumber, DEC));
-  (void) Serial.println(String("..."));
+  (void) Serial.print("Writing integer number ");
+  (void) Serial.print(integerNumber, DEC);
+  (void) Serial.println("...");
   packet.writeInt(integerNumber);
-  (void) Serial.print(String("Writing first byte "));
-  (void) Serial.print(String(firstByte, DEC));
-  (void) Serial.println(String("..."));
+  (void) Serial.print("Writing first byte ");
+  (void) Serial.print(firstByte, DEC);
+  (void) Serial.println("...");
   packet.writeByte(firstByte);
   // This last write will fail.
-  (void) Serial.print(String("Writing second byte "));
-  (void) Serial.print(String(secondByte, DEC));
-  (void) Serial.println(String(" (this will fail)..."));
+  (void) Serial.print("Writing second byte ");
+  (void) Serial.print(secondByte, DEC);
+  (void) Serial.println(" (this will fail)...");
   packet.writeByte(secondByte);
   if (packet.triedToWriteBeyondCapacity())
   {
-    (void) Serial.println(String("Tried to write beyond packet capacity!"));
+    (void) Serial.println("Tried to write beyond packet capacity!");
   }
   // Print the packet contents.
-  (void) Serial.print(String("Packet contents: "));
+  (void) Serial.print("Packet contents: ");
   (void) Serial.println(packet);
   // Read the contents of the packet data field by field.
-  (void) Serial.println(String("Reading the packet data field by field..."));
+  (void) Serial.println("Reading the packet data field by field...");
   packet.rewind();
-  (void) Serial.print(String("Secondary header: "));
+  (void) Serial.print("Secondary header: ");
   (void) Serial.println(packet.readSecondaryHeader());
-  (void) Serial.print(String("Floating-point number: "));
-  (void) Serial.println(String(packet.readFloat(), 4));
-  (void) Serial.print(String("Integer number: "));
-  (void) Serial.println(String(packet.readInt(), DEC));
-  (void) Serial.print(String("First byte: "));
-  (void) Serial.println(String(packet.readByte(), DEC));
-  (void) Serial.print(String("Second byte (this will fail and return dummy data): "));
-  (void) Serial.println(String(packet.readByte(), DEC));
+  (void) Serial.print("Floating-point number: ");
+  (void) Serial.println(packet.readFloat(), 4);
+  (void) Serial.print("Integer number: ");
+  (void) Serial.println(packet.readInt(), DEC);
+  (void) Serial.print("First byte: ");
+  (void) Serial.println(packet.readByte(), DEC);
+  (void) Serial.print("Second byte (this will fail and return dummy data): ");
+  (void) Serial.println(packet.readByte(), DEC);
   if (packet.triedToReadBeyondLength())
   {
-    (void) Serial.println(String("Tried to read beyond packet capacity!"));
+    (void) Serial.println("Tried to read beyond packet capacity!");
   }
   // Print the raw packet contents.
-  (void) Serial.print(String("Hexadecimal dump of the raw packet contents: "));
+  (void) Serial.print("Hexadecimal dump of the raw packet contents: ");
   byte rawPacketBytesArray[packet.length()];
   ESAT_Buffer rawPacketBytes(rawPacketBytesArray, packet.length());
   (void) packet.writeTo(rawPacketBytes);
   (void) Serial.println(rawPacketBytes);
   // End.
-  (void) Serial.println(String("End."));
-  (void) Serial.println(String(""));
+  (void) Serial.println("End.");
+  (void) Serial.println("");
   delay(1000);
 }
